da7dc3dc03ed0671691481e944bfdfa68f925c82
Alladio Bonesso <13860253+alladiobb@users.noreply.github.com>
2024-01-14 12:26:28 -0300

    Add: Add others list methods

--- 01 - Modelagem.c
+++ 01 - Modelagem.c
@@ -27,16 +27,49 @@ typedef struct{
 //     Para inicializar uma estrutura qualquer, precusanis oebsar bis vakires adequados para cada um dos campos da nossa estrutura
 //         Para inicializar um lista sequencial já criada pelo usuário, só precisamo colocar o valor 0(zero) no número de de elementos válidos
 
-void inicializarLista(LISTA 1){
-    1.nroElem=0;
+void inicializarLista(LISTA l){
+    l.nroElem=0;
 }
 
 //Há algum problema com este código?
 //Qual a diferença entre os códigos?
 
-void inicializarLista(LISTA* 1){
-    1->nroElem=0;
+void inicializarLista(LISTA* l){
+    l->nroElem=0;
 }
 
 //Na primeira função o parametro é uma lista
 //Na segunda é o endereço de umas lista
+
+//Retornar numero de elementos
+int tamanho(LISTA* l){
+    return l -> nroElem;
+}
+
+//Exibição/Impressão
+void exibirLista(LISTA* l){
+    int i;
+    printf("Lista: \" ");
+    for(i=0;i<l->nroElem;i++){
+        printf("%i",l->A[i].chave)
+    }
+    print("\"\n")
+}
+
+//Buscar por elemento
+
+void buscaSequencial(LISTA* l TIPOCHAVE ch){
+    int i=0;
+    while(i<l->nroElem){
+        if(ch == 1->A[i].chave) return i;
+        else i++;
+    }
+    return -1;
+}
+
+//Inserção de um elemento em posição especifica
+
+bool inserirEleLista(LISTA* l, REGISTRO reg, int i){
+    int j;
+    if((l->nroElem))
+}
