5a7391dcb63c972d58b1423ddc5873fb0755ecbe
Alladio Bonesso <13860253+alladiobb@users.noreply.github.com>
2024-01-15 15:51:58 -0300

    Refact: Name image Add: Linked List

---
+++ 05 - Lista ligada.c
@@ -0,0 +1,8 @@
+//  Ainda é um estrutura linear
+//  Cada elemento possui no máximo um predecessor e um sucessor
+//  A ordem lógica dos elementos (a ordem "vista" pelo usuário) não é a mesma ordem física (em memória principal) dos elementos.
+//  Cada elemento precis indicar quem é o seu sucessor
+
+//  Chamaremos de lista ligada implementação estática, porque nosso regstros serão armazenados em um arranjo criado inicialmente.
+//  Adicionalmente, cada elemento da nossal lista terá um campo para indicar a posição (no arranjo) de seu sucessor.
+
