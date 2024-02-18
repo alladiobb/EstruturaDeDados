04d5c4dbb4baa61d3d4aa7742ff7dff6ffd59a25
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-02-18 08:26:25 -0300

    Refact: Malloc Function

--- 00 - Função Malloc.c
+++ 00 - Função Malloc.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <malloc.h>
-int main(){
-  int* y = (int*) malloc(sizeof(int));
-  *y =20;
-  int z = sizeof(int);
+
+
+int main(int argc, char const *argv[])
+{
+ int* y = (int*) malloc(sizeof(int));
+  *y =10;
+  int z = sizeof(char);
   printf("*y=%i z=%i\n", *y, z);
   return 0;
 }
