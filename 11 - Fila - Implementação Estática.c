4abde607e6ddd8e7f5937a7ce473e95f9afd6e3a
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-02-21 01:43:56 -0300

    Add: Inicializar, Tamanho and ImprimirFila

--- 11 - Fila - Implementação Estática.c
+++ 11 - Fila - Implementação Estática.c
@@ -1,7 +1,42 @@
 #include<stdio.h>
 #include<malloc.h>
 
+#define MAX 50
 #define true 1
 #define false 0
-typedef int bool;
 
+typedef int bool;
+typedef int Chave;
+
+typedef struct{
+    Chave chave;
+}REGISTRO;
+
+typedef struct{
+    REGISTRO registros[MAX];
+    int inicio;
+    int numeroElementos;
+}FILA;
+
+void inicializa(FILA* fila ){
+    fila->inicio = 0;
+    fila->numeroElementos = 0;
+}
+
+int tamanho(FILA* fila){
+    return fila->numeroElementos;
+}
+
+int imprimirFila(FILA* fila){
+    printf("Fila: \" ");
+    int i = fila->inicio;
+    int temp;
+    for (temp = 0; temp < fila->numeroElementos; temp++)
+    {
+        printf("%i ", fila->registros[i].chave);
+        //Essa linha abaixo é crucial para o funcionamento da função imprimirFila.
+        //Ela garante que o loop percorra todos os elementos da fila sem acessar elementos fora do array.
+        i = (i + 1) % MAX;
+    }
+    printf("\"n");
+}
