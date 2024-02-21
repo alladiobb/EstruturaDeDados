fd4f37a1157dfbb5e0dd2ba9c2afd60f73b5e27a
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-02-21 07:06:26 -0300

    Refact: Structure(define bool, true and false), l:lista

--- 01 - Modelagem - Lista Linear Sequencial.c
+++ 01 - Modelagem - Lista Linear Sequencial.c
@@ -1,6 +1,10 @@
+#define true 1
+#define false 0
 #define MAX 50
 
-type int TIPOCHAVE;
+typedef int bool;
+
+typedef int TIPOCHAVE;
 
 typedef struct{
     TIPOCHAVE chave;
@@ -27,63 +31,63 @@ typedef struct{
 //     Para inicializar uma estrutura qualquer, precusanis oebsar bis vakires adequados para cada um dos campos da nossa estrutura
 //         Para inicializar um lista sequencial já criada pelo usuário, só precisamo colocar o valor 0(zero) no número de de elementos válidos
 
-void inicializarLista(LISTA l){
-    l.nroElem=0;
+void inicializarLista(LISTA lista){
+    lista.nroElem=0;
 }
 
 //Há algum problema com este código?
 //Qual a diferença entre os códigos?
 
-void inicializarLista(LISTA* l){
-    l->nroElem=0;
+void inicializarLista(LISTA* lista){
+    lista->nroElem=0;
 }
 
 //Na primeira função o parametro é uma lista
 //Na segunda é o endereço de umas lista
 
 //Retornar numero de elementos
-int tamanho(LISTA* l){
+int tamanho(LISTA* lista){
     return l -> nroElem;
 }
 
 //Exibição/Impressão
-void exibirLista(LISTA* l){
+void exibirLista(LISTA* lista){
     int i;
     printf("Lista: \" ");
-    for(i=0;i<l->nroElem;i++){
-        printf("%i",l->A[i].chave);
+    for(i=0;i<lista->nroElem;i++){
+        printf("%i",lista->A[i].chave);
     }
     print("\"\n");
 }
 
 //Buscar por elemento
-void buscaSequencial(LISTA* l TIPOCHAVE ch){
+int buscaSequencial(LISTA* lista, TIPOCHAVE ch){
     int i=0;
-    while(i<l->nroElem){
-        if(ch == 1->A[i].chave) return i;
+    while(i<lista->nroElem){
+        if(ch == lista->A[i].chave) return i;
         else i++;
     }
     return -1;
 }
 
 //Inserção de um elemento em posição especifica
-bool inserirElemLista(LISTA* l, REGISTRO reg, int i){
+bool inserirElemLista(LISTA* lista, REGISTRO reg, int i){
     int j;
-    if((l->nroElem == MAX) || (i<0) || (i > l->nroElem))
+    if((lista->nroElem == MAX) || (i<0) || (i > lista->nroElem))
         return false;
-    for (j = l->nroElem; j>i ; j--) l-> A[j] = l -> A[j-1];
-    l -> A[i] = reg;
-    l -> nroElem++;
+    for (j = lista->nroElem; j>i ; j--) lista-> A[j] = lista -> A[j-1];
+    lista -> A[i] = reg;
+    lista -> nroElem++;
     return true;
 }
 
 //Exclusão Elemento
-bool excluirElemLista(TIPOCHAVE ch, LISTA* 1){
+bool excluirElemLista(TIPOCHAVE ch, LISTA* lista){
     int pos, j;
-    pos = buscaSequencial(l,ch);
+    pos = buscaSequencial(lista,ch);
     if(pos == -1) return false;
-    for (j = pos; j < l->nroElem-1; j++)
-        l -> A[j] = l -> A[j+1];
-    l->nroElem--;
+    for (j = pos; j < lista->nroElem-1; j++)
+        lista -> A[j] = lista -> A[j+1];
+    lista->nroElem--;
     return true;
 }
